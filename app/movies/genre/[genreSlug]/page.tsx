0624c501aaffe3ea31453294a4c0dcbf5cae404e
Hamzin M. <bersfromhell@gmail.com>
2025-05-05 17:46:21 +0200

    fix

---
+++ app/movies/genre/[genreSlug]/page.tsx
@@ -0,0 +1,3 @@
+export default function MovieGenrePage() {
+    return <div>MovieGenrePage</div>
+}
---
+++ app/tvshows/genre/[genreSlug]/page.tsx
@@ -0,0 +1,3 @@
+export default function TvShowGenrePage() {
+    return <div>MovieGenrePage</div>
+}
